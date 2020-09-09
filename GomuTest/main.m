7446f46611cdf40da8ff3441f9bb1a89f7610875
674313234@qq.com <674313234@qq.com>
2020-09-09 16:01:03 +0800

    malloc

---
+++ GomuTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GomuTest
+//
+//  Created by 庞彬 on 2020/9/9.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
